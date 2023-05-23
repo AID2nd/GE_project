2949ac229a9a452e936ff37b37c4460158996a55
jiyong_98 <88379533+jiyong98@users.noreply.github.com>
2023-05-24 05:04:05 +0900

    아군AI 컨트롤러 셋팅

--- 코딩/소스코드/아군ai/ourAIController.h
+++ 코딩/소스코드/아군ai/ourAIController.h
@@ -13,5 +13,13 @@ UCLASS()
 class TEST2_API AourAIController : public AAIController
 {
 	GENERATED_BODY()
+public:
+	void getAccumulateData(); //누적 데이터를 얻어오는 함수
+	void unlockSkill(int unlockSkillNumber, int getItem); // 특정 아이템이 있을 시 해당 스킬을 해금하는 함수
+	void avoidSkill(int getAttackedSkillNumber); // 보스에게 특정 스킬공격받을시 피하는 함수
+private:
+	int32 itemUpDamage; // 데미지 아이템에따른 상승량 
+	int32 unlockSkillNumber; // 저장된 해금용 스킬
+	int32 itemNumber; // 얻어온 아이템 저장소
 	
 };
