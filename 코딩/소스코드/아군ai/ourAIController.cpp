11f390958c70b49fe2586b738d926c9e242cecb7
jiyong_98 <88379533+jiyong98@users.noreply.github.com>
2023-05-28 18:58:45 +0900

    아군ai강화 코드(1)

--- 코딩/소스코드/아군ai/ourAIController.cpp
+++ 코딩/소스코드/아군ai/ourAIController.cpp
@@ -2,17 +2,38 @@
 
 
 #include "ourAIController.h"
-
+#include "Blade_Character.h"
 
 void getAccumulateData()
 {
-
+	//누적데미지 받아오기
+	Blade_Character* Blade_Character = Cast<Blade_Character>(OtherActor);
+	getAccumulateData = Blade_Character->getAccumulateData();
 }
 
 void AourAIController::unlockSkill(int unlockSkillNumber, int getItem)
 {
+	
 }
 
 void AourAIController::avoidSkill(int getAttackedSkillNumber)
+{
+}
+
+void AourAIController::upDamage()
+{
+	if (getUpAditem)
+	{
+		//해당 아이템이 있으면 데미지증가
+		itemUpDamage += 10;
+	}
+	if (getUpSdItem)
+	{
+		//방어력증가
+		itemUpDefense += 10;
+	}
+}
+
+void AourAIController::upDefense()
 {
 }
--- 코딩/소스코드/아군ai/ourAIController.h
+++ 코딩/소스코드/아군ai/ourAIController.h
@@ -17,9 +17,18 @@ public:
 	void getAccumulateData(); //누적 데이터를 얻어오는 함수
 	void unlockSkill(int unlockSkillNumber, int getItem); // 특정 아이템이 있을 시 해당 스킬을 해금하는 함수
 	void avoidSkill(int getAttackedSkillNumber); // 보스에게 특정 스킬공격받을시 피하는 함수
+
+	void upDamage();
+	void upDefense();
 private:
 	int32 itemUpDamage; // 데미지 아이템에따른 상승량 
+	int32 itemUpDefense; //방어력상승
 	int32 unlockSkillNumber; // 저장된 해금용 스킬
 	int32 itemNumber; // 얻어온 아이템 저장소
+
+	bool getUpAditem; //공격력아이템
+	bool getUpSdItem; //방어력아이템
+
+	int AccumulateDamage;
 	
 };
